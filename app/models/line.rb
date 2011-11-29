38a0511f1fbdd9dfdad19a0b211da9d0e09736ec
Tom Jakubowski <tom@crystae.net>
2011-11-28 17:01:33 -0800

    Added color validations to Line model.
    The Line model will only accept six-digit hexadecimal color codes, formatted
    like #0F1ca7.

--- app/models/line.rb
+++ app/models/line.rb
@@ -2,4 +2,5 @@ class Line < ActiveRecord::Base
   attr_accessible :name, :color
   
   validates :name, :presence => :true
+  validates_format_of :color, :with => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
 end
--- spec/models/line_spec.rb
+++ spec/models/line_spec.rb
@@ -5,4 +5,14 @@ describe Line do
     nameless = Line.new(:name => "")
     nameless.should_not be_valid
   end
+
+  it "should reject a poorly formatted color" do
+    lazycolor = Line.new(:name => "Gold Line", :color => "gold")
+    lazycolor.should_not be_valid
+  end
+
+  it "should accept a well formatted color" do
+    hexcolor = Line.new(:name => "Gold Line", :color => "#FDB913")
+    hexcolor.should be_valid
+  end
 end
