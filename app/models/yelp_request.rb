1cdba6952c0b77eb9f98491e2aee7542abb0d3c8
Tom Jakubowski <tom@crystae.net>
2011-11-30 19:56:55 -0800

    Refactored some of YelpRequest.
    OAUTH keys, secrets, and tokens are now instance variables of the YelpRequest.
    Also now, 'best match' sorting is used rather than distance.

--- app/models/yelp_request.rb
+++ app/models/yelp_request.rb
@@ -2,24 +2,26 @@ require 'rubygems'
 require 'oauth'
 
 class YelpRequest
+  attr_accessor :consumer_key, :consumer_secret, :token, :token_secret, :api_host
+
   def initialize(term, latitude, longitude, limit, radius)
     @term = term
     @ll = "#{latitude},#{longitude}"
     @limit = limit
     @radius = radius
+
+    @consumer_key = ENV['YELP_OAUTH_CONSUMER_KEY']
+    @consumer_secret = ENV['YELP_OAUTH_CONSUMER_SECRET']
+    @token = ENV['YELP_OAUTH_TOKEN']
+    @token_secret = ENV['YELP_OAUTH_TOKEN_SECRET']
+    @api_host = 'api.yelp.com'
   end
 
   def fetch_raw_data
-    consumer_key = ENV['YELP_OAUTH_CONSUMER_KEY']
-    consumer_secret = ENV['YELP_OAUTH_CONSUMER_SECRET']
-    token = ENV['YELP_OAUTH_TOKEN']
-    token_secret = ENV['YELP_OAUTH_TOKEN_SECRET']
-    api_host = 'api.yelp.com'
+    consumer = OAuth::Consumer.new(@consumer_key, @consumer_secret, {:site => "http://#{@api_host}"})
+    access_token = OAuth::AccessToken.new(consumer, @token, @token_secret)
 
-    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-    access_token = OAuth::AccessToken.new(consumer, token, token_secret)
-
-    path = "/v2/search?term=#{@term}&ll=#{@ll}&limit=#{@limit}&sort=1&radius_filter=#{@radius}"
+    path = "/v2/search?term=#{@term}&ll=#{@ll}&limit=#{@limit}&sort=0&radius_filter=#{@radius}"
     access_token.get(path).body
   end
 end
--- spec/models/yelp_request_spec.rb
+++ spec/models/yelp_request_spec.rb
@@ -1,6 +1,26 @@
 require 'spec_helper'
 
 describe YelpRequest do
+  before(:each) do
+    @dummy = YelpRequest.new("pizza", 0.0, 0.0, 5, 1000)
+  end
+
+  it "should have a consumer key" do
+    @dummy.consumer_key.should_not be_nil
+  end
+
+  it "should have a consumer secret" do
+    @dummy.consumer_secret.should_not be_nil
+  end
+
+  it "should have a token" do
+    @dummy.token.should_not be_nil
+  end
+
+  it "should have a token secret" do
+    @dummy.token_secret.should_not be_nil
+  end
+
   describe "successful request" do
     use_vcr_cassette "pizza_union_station", :record => :new_episodes
 
