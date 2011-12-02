6de29d280701ed4b191d7ccfc99830096cbd80d2
Tom Jakubowski <tom@crystae.net>
2011-12-02 11:06:27 -0800

    short_station_listing helper now provides a link tag to the station page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,10 @@
 module ApplicationHelper
   def short_station_listing(station, line=nil)
-    listing = station.name
-    if (line)
-      lines = station.transfers_from(line)
-    else
-      lines = station.lines_served
-    end
+    listing = link_to(station.name, station, :class => "camo")
+    lines = line ? station.transfers_from(line) : station.lines_served
+
     colors = lines.map { |l| l.color }
-    listing << " #{colors.map {|c| colored_dot(c)}.join('')}" if colors.length > 0
+    listing << " #{colors.map {|c| colored_dot(c)}.join('')}".html_safe if colors.length > 0
     listing.html_safe
   end
 
