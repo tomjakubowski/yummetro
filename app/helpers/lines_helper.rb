1e39ca6cbde0de1a6ebf2e67a66d023eb222b436
Tom Jakubowski <tom@crystae.net>
2011-12-01 16:01:38 -0800

    Refactored short_station_listing a bit.

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -2,9 +2,7 @@ module LinesHelper
   def short_station_listing(station, line)
     listing = station.name
     transfers = station.transfers_from(line)
-    if (transfers.length > 0)
-      transfers.each { |t| listing << " #{color_indicator(t)}"}
-    end
+    listing << " #{transfers.map {|t| color_indicator(t)}.join('')}" if transfers.length > 0
     listing.html_safe
   end
 
