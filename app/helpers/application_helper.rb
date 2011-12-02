8fbf164db70ed31b44279d390ceaff2cfdff648d
Tom Jakubowski <tom@crystae.net>
2011-12-01 23:51:06 -0800

    Refactored station listing helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def short_station_listing(station, line=nil)
+    listing = station.name
+    if (line)
+      lines = station.transfers_from(line)
+    else
+      lines = station.lines_served
+    end
+    colors = lines.map { |l| l.color }
+    listing << " #{colors.map {|c| colored_dot(c)}.join('')}" if colors.length > 0
+    listing.html_safe
+  end
+
+  def colored_dot(color)
+    "<span style=\"color: #{color};\">&#9679;</span>".html_safe
+  end
 end
--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,12 +1,5 @@
 module LinesHelper
-  def short_station_listing(station, line)
-    listing = station.name
-    transfers = station.transfers_from(line)
-    listing << " #{transfers.map {|t| color_indicator(t)}.join('')}" if transfers.length > 0
-    listing.html_safe
-  end
-
   def color_indicator(line)
-    "<span style=\"color: #{line.color};\">&#9679;</span>".html_safe
+    colored_dot(line.color)
   end
 end
