79ee416dbf435032d302ff0a4669ea2f0ef64c77
Tom Jakubowski <tom@crystae.net>
2011-12-04 16:57:17 -0800

    Various additions to search.
    SearchController finally has a spec!
    
    Search results page shows the "Powered by Yelp!" image, as required by
    the Yelp API Terms of Use.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,7 @@
 module SearchHelper
+  def powered_by_yelp
+    image_url = "http://media3.ak.yelpcdn.com/static/201012161285253944/img/developers/Powered_By_Yelp_Red.png"
+    link_url = "http://www.yelp.com/"
+    link_to image_tag(image_url, :alt => "Powered by Yelp!"), link_url, :target => "_blank"
+  end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,56 @@
 require 'spec_helper'
 
 describe SearchController do
-  pending "tests go here"
+  render_views
+
+  describe "Successful station search" do
+    use_vcr_cassette "pizza_union_station", :record => :new_episodes
+
+    before(:each) do
+      @station = Factory(:station)
+      @term = "pizza"
+      @params = {:query => @term, :near => @station.name}
+    end
+
+    it "should show the search term in the heading" do
+      get :search, @params
+      response.should have_selector("h1", :content => @term)
+    end
+
+    it "should show the station name in the heading" do
+      get :search, @params
+      response.should have_selector("h1", :content => @station.name)
+    end
+
+    it "should show the search results" do
+      results = Search.new(:term => @term, :station => @station).fetch_results
+
+      get :search, @params
+      results.each do |r|
+        response.should have_selector(".search-result", :content => r.name)
+      end
+    end
+
+    describe "Yelp! credits" do
+      it "should display the Powered by Yelp! image" do
+        get :search, @params
+        response.should have_selector(".api-credits img")
+      end
+
+      it "should have an alt text" do
+        get :search, @params
+        response.should have_selector(".api-credits img", :alt => "Powered by Yelp!")
+      end
+
+      it "should link to the Yelp! website" do
+        get :search, @params
+        response.should have_selector(".api-credits a", :href => "http://www.yelp.com/")
+      end
+    end
+  end
+
+  describe "Failed station search" do
+    pending "tests go here"
+  end
+
 end
